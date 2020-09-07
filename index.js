34fc4de516101140d05679671f3499958de897d3
Ratish Bansal <ratbansa@adobe.com>
2020-09-08 00:30:55 +0530

    Refactor project structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log('Example app listening on port 8080!')
+});
