89d941241d20f23d9a056336d82ba64a162f2f19
Ratish Bansal <ratbansa@adobe.com>
2020-09-08 00:01:30 +0530

    heroku based config

--- src/myServer.js
+++ src/myServer.js
@@ -1,4 +1,8 @@
 var http = require("http");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
 http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
@@ -7,4 +11,4 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(port);
